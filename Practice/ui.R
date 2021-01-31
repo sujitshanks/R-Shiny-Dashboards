bcbb6622620caea13ff9c4612fc74d3ae03d7d2e
KikoFlorenti <sujitshankar1996@yahoo.co.in>
2021-01-31 16:32:19 +0530

    Testing sliders on sample histogram

--- Practice/ui.R
+++ Practice/ui.R
@@ -5,12 +5,18 @@ shinyUI(
   dashboardPage(
     dashboardHeader(title = "Shanks Backyard"),
     dashboardSidebar(
+      sliderInput("bins","Number of Breaks", 1, 100, 50),
       menuItem("Dashboard"),
          menuSubItem("Dashboard: Finance"),
          menuSubItem("Dashboard: Sales"),
       menuItem("Detailed Analysis"),
       menuItem("Raw Data")
     ),
-    dashboardBody()
+    dashboardBody(
+      #everything in the body needs to be in a box
+      fluidRow(
+        box(plotOutput("histogram"))
+      )
+    )
    )
 )
